5606fe1ade93e62d08c338887389a396198f2fcd
Salma Abdelrahman <104564028+salma-a11y@users.noreply.github.com>
2024-12-15 20:31:55 +0200

    Update Program.fs
    handle add book to prevent add the same book twice

--- lastVersion/lipraryMSp/Program.fs
+++ lastVersion/lipraryMSp/Program.fs
@@ -36,10 +36,13 @@ let loadLibraryFromFile () =
 
 // Function to add a new book
 let addBook title author genre =
-    let book = { Title = title; Author = author; Genre = genre; IsBorrowed = false; BorrowDate = None }
-    library <- library.Add(title, book)
-    saveLibraryToFile ()
-    "Book added successfully"
+    if library.ContainsKey(title) then
+        "This book already exists in the library."
+    else
+        let book = { Title = title; Author = author; Genre = genre; IsBorrowed = false; BorrowDate = None }
+        library <- library.Add(title, book)
+        saveLibraryToFile ()
+        "Book added successfully"
 
 // Function to search for a book by title
 let searchBook title =
